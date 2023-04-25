910df01cb032deb2407c9c9e724122b5f54fe7e1
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-04-25 20:39:21 +0100

    Remove unused policy and add repo for preservica client

--- root.tf
+++ root.tf
@@ -1,26 +1,16 @@
 module "github_environments_repository" {
   source          = "./repositories"
-  repository_name = "dri-terraform-github-environments"
+  repository_name = "dp-terraform-github-environments"
   language        = "Scala"
-  checks          = ["terraform-check:0"]
+  checks          = ["test:test"]
   team_name       = "digital-records-infrastructure"
   team_permission = "admin"
-}
-
-module "repository_role" {
-  source = "git::https://github.com/nationalarchives/tdr-terraform-modules.git//iam_role"
-  name   = "DRITerraformRepositoriesRoleMgmt"
-  common_tags = {
-    Name = "DRITerraformRepositoriesRoleMgmt"
+  secrets = {
+    WORKFLOW_TOKEN    = data.aws_ssm_parameter.github_workflow_token.value
+    SLACK_WEBHOOK     = data.aws_ssm_parameter.github_slack_webhook.value
+    SONATYPE_USERNAME = data.aws_ssm_parameter.github_sonatype_username.value
+    SONATYPE_PASSWORD = data.aws_ssm_parameter.github_sonatype_password.value
+    GPG_PRIVATE_KEY   = data.aws_ssm_parameter.github_gpg_key.value
+    GPG_PASSWORD      = data.aws_ssm_parameter.github_gpg_passphrase.value
   }
-  assume_role_policy = templatefile("./templates/role/github_assume_role.json.tpl", { account_id = data.aws_caller_identity.current.account_id })
-  policy_attachments = {
-    access_state_s3_bucket = module.repository_policy.policy_arn
-  }
-}
-
-module "repository_policy" {
-  source        = "git::https://github.com/nationalarchives/tdr-terraform-modules.git//iam_policy"
-  name          = "DRITerraformRepositoriesPolicyMgmt"
-  policy_string = templatefile("./templates/policy/terraform_policy.json.tpl", { bucket_name = local.bucket_name, account_id = data.aws_caller_identity.current.account_id })
 }
--- root_backend.tf
+++ root_backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "dri-terraform-state-store"
-    key    = "terraform.github.state"
-    region = "eu-west-2"
+    bucket         = "mgmt-dp-terraform-state"
+    key            = "terraform.github.state"
+    region         = "eu-west-2"
+    dynamodb_table = "mgmt-dp-terraform-state-lock"
   }
 }
