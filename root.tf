3d4d469567f0d14e8721a244234d7cec8c9b73f2
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-05-04 17:47:52 +0100

    Add dp-preservica-schemas-repo

--- root.tf
+++ root.tf
@@ -14,3 +14,16 @@ module "github_environments_repository" {
     GPG_PASSPHRASE    = data.aws_ssm_parameter.github_gpg_passphrase.value
   }
 }
+
+module "github_schemas_repository" {
+  source          = "./repositories"
+  repository_name = "dp-preservica-schemas"
+  language        = "Python"
+  checks          = ["test:0"]
+  team_name       = "digital-records-infrastructure"
+  team_permission = "admin"
+  secrets = {
+    MANAGEMENT_ACCOUNT = data.aws_caller_identity.current.account_id
+    SLACK_WEBHOOK      = data.aws_ssm_parameter.github_slack_webhook.value
+  }
+}
