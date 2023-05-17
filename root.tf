8c1e5dd8e6e0db11436c3d58f410da8026747050
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-05-17 12:46:24 +0100

    Stop creating the repos and add the da-aws-clients
    I've changed this to stop creating the repos in terraform as one
    mistimed destroy will delete everything from these repos.
    
    I've changed it to use the tdr terraform module which gets the repo as a
    data source and adds the secrets in. This lets us manage the secrets
    properly without wiping out the repo accidentally.
    
    I've also added the da-aws-clients repo. Technically it's a cross
    department repo but it can live here for now.

--- root.tf
+++ root.tf
@@ -1,10 +1,19 @@
-module "github_environments_repository" {
-  source          = "./repositories"
-  repository_name = "dp-preservica-client"
-  language        = "Scala"
-  checks          = ["test:0"]
-  team_name       = "digital-records-infrastructure"
-  team_permission = "admin"
+module "github_preservica_client_repository" {
+  source          = "git::https://github.com/nationalarchives/tdr-terraform-modules//github_repositories"
+  repository_name = "nationalarchives/dp-preservica-client"
+  secrets = {
+    WORKFLOW_TOKEN    = data.aws_ssm_parameter.github_workflow_token.value
+    SLACK_WEBHOOK     = data.aws_ssm_parameter.github_slack_webhook.value
+    SONATYPE_USERNAME = data.aws_ssm_parameter.github_sonatype_username.value
+    SONATYPE_PASSWORD = data.aws_ssm_parameter.github_sonatype_password.value
+    GPG_PRIVATE_KEY   = data.aws_ssm_parameter.github_gpg_key.value
+    GPG_PASSPHRASE    = data.aws_ssm_parameter.github_gpg_passphrase.value
+  }
+}
+
+module "github_aws_clients_repository" {
+  source          = "git::https://github.com/nationalarchives/tdr-terraform-modules//github_repositories"
+  repository_name = "nationalarchives/da-aws-clients"
   secrets = {
     WORKFLOW_TOKEN    = data.aws_ssm_parameter.github_workflow_token.value
     SLACK_WEBHOOK     = data.aws_ssm_parameter.github_slack_webhook.value
@@ -16,15 +25,10 @@ module "github_environments_repository" {
 }
 
 module "github_preservica_config_repository" {
-  source          = "./repositories"
-  repository_name = "dp-preservica-config"
-  language        = "Scala"
-  checks          = ["test:0"]
-  team_name       = "digital-records-infrastructure"
-  team_permission = "admin"
+  source          = "git::https://github.com/nationalarchives/tdr-terraform-modules//github_repositories"
+  repository_name = "nationalarchives/dp-preservica-config"
   secrets = {
     MANAGEMENT_ACCOUNT = data.aws_caller_identity.current.account_id
     SLACK_WEBHOOK      = data.aws_ssm_parameter.github_slack_webhook.value
   }
 }
-
