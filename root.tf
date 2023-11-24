c5d7e82f0b7799e1428c8b6ff33a4cea8055c1a7
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-11-24 14:02:05 +0000

    DR2-1350 Create s3 copy repo secrets

--- root.tf
+++ root.tf
@@ -366,3 +366,25 @@ module "ingest_parent_folder_opex_creator_environments" {
     ACCOUNT_NUMBER = each.value
   }
 }
+
+module "ingest_s3_copy_lambda" {
+  source          = "git::https://github.com/nationalarchives/da-terraform-modules//github_repository_secrets"
+  repository_name = "nationalarchives/dr2-s3-copy"
+  secrets = {
+    MANAGEMENT_ACCOUNT = data.aws_caller_identity.current.account_id
+    SLACK_WEBHOOK      = data.aws_ssm_parameter.github_slack_webhook.value
+    WORKFLOW_TOKEN     = data.aws_ssm_parameter.github_workflow_token.value
+  }
+}
+
+module "ingest_s3_copy_environments" {
+  for_each              = module.configuration.account_numbers
+  source                = "git::https://github.com/nationalarchives/da-terraform-modules//github_environment_secrets"
+  environment           = each.key
+  repository_name       = "nationalarchives/dr2-s3-copy"
+  team_slug             = "digital-records-repository"
+  integration_team_slug = []
+  secrets = {
+    ACCOUNT_NUMBER = each.value
+  }
+}
