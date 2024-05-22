19fbcecdf4b386f7ad64e53973ac997a8b6e5f22
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-05-22 14:16:46 +0100

    Add GPG key to sign commits
    We're commiting the version bump change in the repo but the commit isn't
    being signed. We need the gpg key to sign it.

--- root.tf
+++ root.tf
@@ -69,6 +69,8 @@ module "disaster_recovery" {
     MANAGEMENT_ACCOUNT = data.aws_caller_identity.current.account_id
     SLACK_WEBHOOK      = data.aws_ssm_parameter.github_slack_webhook.value
     WORKFLOW_TOKEN     = data.aws_ssm_parameter.github_workflow_token.value
+    GPG_PRIVATE_KEY    = data.aws_ssm_parameter.github_gpg_key.value
+    GPG_PASSPHRASE     = data.aws_ssm_parameter.github_gpg_passphrase.value
   }
 }
 
